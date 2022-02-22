deadd0622d052996826e65132df45597f4658690
kelly-sangoluisa <94008979+kelly-sangoluisa@users.noreply.github.com>
2022-02-21 20:03:37 -0500

    agregar texto

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void lblNombre(object sender, EventArgs e)
+        {
+
+        }
     }
 }
